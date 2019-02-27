f1fb0c7e2cc538b6df5381e370a66f4cc9bdce30
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-27 09:20:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season,holiday|
   puts"#{season.capitalize}:"
   holiday.each do |key,value|
-    key.to_s.split("_").each{|item| item.capitalize}.join(" ")
-    
+
     puts"  #{key.to_s.split("_").each{|item| item.capitalize}.join(" ")}: #{value.join(", ")}"
 end
 end
