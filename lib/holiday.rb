bff8e62a99b4dcaded87f7a213c14b5398ea798d
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-27 07:48:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,10 +56,9 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   
   holiday_hash.each do |season, holiday|
-    holiday.each do |key, value|
     if(season==:winter)
       
-    end
+    season.values
   end
     
   end
