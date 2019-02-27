72d89fc3f0f290d83be1b6f81f5dfde5c9c18118
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-27 08:52:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,24 +1,23 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  
-  #given that holiday_hash looks like this:
-   {
-     :winter => {
-       :christmas => ["Lights", "Wreath"],
-       :new_years => ["Party Hats"]
-     },
-     :summer => {
-       :fourth_of_july => ["Fireworks", "BBQ"]
-     },
-     :fall => {
-       :thanksgiving => ["Turkey"]
-     },
-     :spring => {
-       :memorial_day => ["BBQ"]
-     }
-   }
-   #return the second element in the 4th of July array
+  # given that holiday_hash looks like this:
+  # {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
+  # }
+  # return the second element in the 4th of July array
   
   holiday_hash[:summer][:fourth_of_july][1]
 end
@@ -71,7 +70,6 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season,holiday|
   holiday.each do |key,value|
     puts"#{season.capitalize}:"
-    binding.pry
 end
 end
 end
