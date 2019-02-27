a25c266f3df55c54eb0890036c6d9c53181bfa90
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-27 09:06:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,9 +68,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season,holiday|
-  puts"#{season.capitalize}:"
+  "#{season.capitalize}:"
   holiday.each do |key,value|
-    puts"  #{key.capitalize}: #{value.join(", ")}"
+    "  #{key.capitalize}: #{value.join(", ")}"
 end
 end
 end
